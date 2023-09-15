0222f695fc5236bcf258ab77bf30a43da1fd3b4a
koushikwebdev <kou.saha.ami@gmail.com>
2023-09-15 21:05:00 +0530

    add to cart

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log("Connected");
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 5,
+  spaceBetween: 0,
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: true,
+  },
+});
+
+const mySwipper = document.querySelector(".mySwiper");
+mySwipper.addEventListener("mouseenter", () => {
+  mySwipper.swiper.autoplay.stop();
+});
+
+mySwipper.addEventListener("mouseleave", () => {
+  mySwipper.swiper.autoplay.start();
+});
+
+const slides = document.querySelectorAll(".slider-product");
+
+slides.forEach((slide) => {
+  slide.addEventListener("mouseenter", () => {
+    slide.querySelector(".add-cart").classList.add("show");
+  });
+
+  slide.addEventListener("mouseleave", () => {
+    slide.querySelector(".add-cart").classList.remove("show");
+  });
+});
